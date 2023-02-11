73be7e8700aee835a58c07e5b6472732303f51e3
Ilia Reshetov <42715397+iliareshetov@users.noreply.github.com>
2023-02-11 09:36:43 +0100

    initial commit generated by spring init

---
+++ adwize/src/main/java/fi/missingprojectfiles/adwize/AdwizeApplication.java
@@ -0,0 +1,13 @@
+package fi.missingprojectfiles.adwize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdwizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdwizeApplication.class, args);
+    }
+
+}
---
+++ adwize/src/test/java/fi/missingprojectfiles/adwize/AdwizeApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.missingprojectfiles.adwize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdwizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
